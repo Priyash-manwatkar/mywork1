6cb80c138e579f678055c5f9e567da12c3b9fdab
priyash manwatkar <priyashmanwatkar@gmail.com>
2025-01-13 10:18:21 +0530

    fifth commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// let fruits=["Mango","Apple","Pineapple","Orange","grapes","watermelon"];
+// let [fruist1,fruit2,fruit3,...rest]=fruits;
+// console.log(rest);
+// let fruit=[...fruits];
+// console.log(fruit);
+const obj={
+    username:"priyash",
+    surname:"manwatkar",
+    view: function (){
+        let username="Priyash";
+        console.log(this.username)
+    }
+}
+obj.view();
+
